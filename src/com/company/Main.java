57b17e66db9e65877f2e2ded9e67fbe950cd5fb3
JelliedFish <hungrydog757@gmail.com>
2020-03-12 13:31:48 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("It's submodule");
+        System.out.print("Forget it");
+        System.out.print("Submodule...");
+
+    }
+}
